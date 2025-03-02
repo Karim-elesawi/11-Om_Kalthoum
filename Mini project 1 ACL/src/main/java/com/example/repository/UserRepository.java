05ddf9175f9e5b5a17817b21d98673b84fea93fc
Karim-elesawi <karim.h.elesawi@gmail.com>
2025-03-02 16:23:10 +0200

    Add files via upload

---
+++ Mini project 1 ACL/src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,64 @@
+package com.example.repository;
+
+import com.example.model.User;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.UUID;
+import com.example.model.Order;
+import java.util.List;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class UserRepository extends MainRepository<User>{
+    public UserRepository() {
+    }
+
+    @Override
+    protected String getDataPath() {
+        return "src/main/java/com/example/data/users.json";
+    }
+    @Override
+    protected Class<User[]> getArrayType() {
+        return User[].class;
+    }
+
+    public ArrayList<User> getUsers() {
+        return findAll();
+    }
+    public User getUserById(UUID userId) {
+        return getUsers().stream()
+                .filter(user -> user.getId().equals(userId))
+                .findFirst()
+                .orElse(null);
+    }
+    public User addUser(User user) {
+        user.setId(UUID.randomUUID());
+        save(user);
+        return user;
+    }
+
+    public List<Order> getOrdersByUserId(UUID userId) {
+        User user = getUserById(userId);
+        return (user != null) ? user.getOrders() : new ArrayList<>();
+    }
+    public void addOrderToUser(UUID userId, Order order) {
+        User user = getUserById(userId);
+        if (user != null) {
+            user.getOrders().add(order);
+            overrideData(getUsers());
+        }
+    }
+    public void removeOrderFromUser(UUID userId, UUID orderId) {
+        User user = getUserById(userId);
+        if (user != null) {
+            user.getOrders().removeIf(order -> order.getId().equals(orderId));
+            overrideData(getUsers());
+        }
+    }
+
+    public void deleteUserById(UUID userId) {
+        ArrayList<User> users = getUsers();
+        users.removeIf(user -> user.getId().equals(userId));
+        overrideData(users);
+    }
+}
